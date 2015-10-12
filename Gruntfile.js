5cb09bc02ecdb2c148f690397b24684135c8c00c
Indri Muska <indrimuska@gmail.com>
2015-10-12 13:16:10 +0200

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+	
+	// Load plugins
+	require('load-grunt-tasks')(grunt);
+	
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		uglify: {
+			options: {
+				banner: '/*! Angular Counter - v<%= pkg.version %> - <%= pkg.homepage %> - (c) 2015 Indri Muska - MIT */\n'
+			},
+			main: {
+				files: {
+					'js/angular-counter.min.js': ['js/angular-counter.js']
+				}
+			}
+		},
+		'sync-json': {
+			options: {
+				include: ['name', 'description', 'version']
+			},
+			bower: {
+				files: {
+					"bower.json": "package.json"
+				}
+			}
+		}
+	});
+	
+	// Default tasks.
+	grunt.registerTask('default', ['uglify', 'sync-json']);
+	grunt.registerTask('update-patch', ['bumpup:patch', 'default']);
+	
+};
